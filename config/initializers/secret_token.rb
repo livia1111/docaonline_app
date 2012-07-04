c9cfef86bdc7b541f0afa153da6965e8f7b623b6
Cassiano Rocha Kuplich <cassianorocha@gmail.com>
2012-07-04 19:47:06 -0300

    inicial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocaonlineApp::Application.config.secret_token = 'e2145158f164bb3c0db610351f9fd40d36b81faf1cda9c4132bdf05d2d745afc5b63090e9f621c94633c3067523fe2c9970a7dc07148a273cf95528b39a08aff'
